35a915d5647fb8acfbc0db23ed7bc0fa5d8bd974
ישראל דוב לבו <lebow@lebowtech.com>
2011-06-20 16:52:33 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Stye Examples #
+
+These files don't really do any thing, but are here to give an idea of how I write code.
+
+Please don't judge me on some of the messes that you might find in my other projects.
+
+This is really how I write code when I have more than 2 min to sit down in front of textmate.
+
---
+++ yisrael_style.php
@@ -0,0 +1,54 @@
+<?PHP
+
+/**
+* This is an example of my coding style
+*/
+class Style
+{
+    /**
+     * the value
+     *
+     * @var string
+     */
+    private $_value = '';
+    
+    /**
+     * sets up our object
+     *
+     * @param string $val
+     * @return Style the style object 
+     * @author ישראל דוב לבו
+     */
+    function __construct($val=NULL)
+    {
+        if(!empty($val)){
+            $this->_value = $val;
+        }
+        return $this;
+    }
+    
+    /**
+     * displays the class (like to string)
+     *
+     * @param string $return should we return the string , default is to print
+     * @return bool|string the rendered string or false
+     * @author ישראל דוב לבו
+     */
+    public function display($return=false)
+    {
+        //load the template
+        $template = new Template();
+        
+        //set the template value
+        $template->value = $this->_value;
+        
+        //should we print or return ?
+        if (!$return) {
+            echo $template->render();
+        }
+        else {
+            $return = $template->render(); 
+        }
+        return $return;
+    }
+}
